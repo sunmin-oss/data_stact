a0ca9d2b9f78f171b57f32424ca2ceba258dbba8
sunmin-oss <c112152342@nkust.edu.tw>
2025-03-05 13:53:15 +0800

    Create 02.cpp

---
+++ 大二下/資料結構/02. 資料結構作業02/02.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+    int x = 0, y = 0, z = 0;
+    while (true)
+    {
+        cin >> x >> y >> z;
+        if (x == 0 && y == 0 && z == 0)
+        {
+            break;
+        }
+
+        
+    
+      
+    
+    
+    
+    }
+    
+}
+    
+
